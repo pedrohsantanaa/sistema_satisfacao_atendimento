2c6c419210c3d3a4011b496501496c4f2dba27ef
Pedro Santana <97036217+pedrohsantanaa@users.noreply.github.com>
2026-03-18 21:37:04 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sistema de Satisfação</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid p-5 bg-primary text-white text-center">
+                <h2>SISTEMA DE SATISFAÇÃO DE ATENDIMENTO</h2>
+        </div>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'cabecalho.php'?>
+
+        <div class="container-fluid p-5 mt-5 mb-5">
+            <div class="row">
+                <div class="col-sm-4 border text-center">
+                    <a href="agradecimento.php?acao=1"><img src="imagens/otimo.png"></a>
+                    <h2>Ótimo</h2>
+                </div>
+                <div class="col-sm-4 border text-center">
+                    <a href="agradecimento.php?acao=2"><img src="imagens/regular.png"></a>
+                    <h2>Regular</h2>
+                </div>
+                <div class="col-sm-4 border text-center">
+                    <a href="agradecimento.php?acao=3"><img src="imagens/ruim.png"></a>
+                    <h2>Ruim</h2>
+                </div>
+            </div>
+        </div>
+
+<?php include 'rodape.php'?>
